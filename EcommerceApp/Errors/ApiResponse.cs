5ea6b3346149eed6ee03b19a92793ab3c95cf4de
amr2234 <Amr.salama223@outllok.com>
2023-11-22 22:25:39 +0200

    FrontEnd

--- EcommerceApp/Errors/ApiResponse.cs
+++ EcommerceApp/Errors/ApiResponse.cs
@@ -18,10 +18,10 @@
         {
             return statuscode switch
             {
-                400 => "Bad Request, you have made",
-                401 => "Authorized , you are not",
-                404 => "Resource found , it was not",
-                500 => "Errors are the path to the dark side",
+                400 => " Bad Request, you have made",
+                401 => " Authorized , you are not",
+                404 => " Resource found , it was not",
+                500 => " Errors are the path to the dark side",
                 _=> null
             };
 
